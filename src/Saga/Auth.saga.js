eafc3a69f16a779c2ce681080f4d8b6e4fc78d34
prashildhameliya <dhameliyaprashil1290@gmail.com>
2022-08-10 09:59:12 +0530

    chng

--- src/Saga/Auth.saga.js
+++ src/Saga/Auth.saga.js
@@ -1,10 +1,10 @@
 import { call, put, takeEvery, all } from 'redux-saga/effects'
-import { SignUpAPI ,LoginApi} from '../Common/SignUpapi';
+import { SignUpAPI, LoginApi } from '../Common/SignUpapi';
 
 import * as Actiontypes from '../Redux/Actiontypes'
 import { emailVerifyAction, Loggieuser } from '../Redux/Action/AuthAction';
 import { RestateAlert, SetAlert } from '../Redux/Action/alertAction';
-import { history } from '../History';
+import { history } from '../history';
 
 
 function* Signup(action) {
@@ -12,12 +12,12 @@ function* Signup(action) {
       console.log("Signup");
       const user = yield call(SignUpAPI, action.payload);
       console.log(user.payload);
-        yield put(SetAlert({ text: user.payload, color: "success" }));
+      yield put(SetAlert({ text: user.payload, color: "success" }));
       //   yield put(emailVerifyAction(user));
 
    } catch (e) {
-      
-       yield put(SetAlert({ text: e.payload, color: "error" }));
+
+      yield put(SetAlert({ text: e.payload, color: "error" }));
       //  yield put({type: "USER_FETCH_FAILED", message: e.message});
       console.log(e);
    }
@@ -26,16 +26,16 @@ function* loginup(action) {
    try {
       // console.log("Signup"); //
       const user = yield call(LoginApi, action.payload);
-        console.log(user);
-        yield put(Loggieuser(user.payload));
-        history.push("/");
-       yield put(SetAlert({ text: "login successfull", color: "success" }));
-     
-          
+      console.log(user);
+      yield put(Loggieuser(user.payload));
+      history.push("/")
+      yield put(SetAlert({ text: "login successfull", color: "success" }));
+
+
 
    } catch (e) {
-      
-        yield put(SetAlert({ text: e.payload, color: "error" }));
+
+      yield put(SetAlert({ text: e.payload, color: "error" }));
       //  yield put({type: "USER_FETCH_FAILED", message: e.message});
       console.log(e.payload);
    }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Router} from 'react-router-dom'
-import { history } from './History';
+import { history } from './history';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
